be03a9e5e6a1aac4796a519d9eecbb4b408b9a1a
Robert Newman <robertnewman@roberts-mbp.home>
2021-06-18 15:36:04 +0100

    jsx for rendering posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,10 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch => {
     const res = await jsonPlaceholder.get('/posts')
-    
+
     dispatch ({
         type: "FETCH_POSTS",
-        payload: res
+        payload: res.data
     });
 };
 
